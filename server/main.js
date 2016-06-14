7cc963ffcdca7955aa82b7a9ad8aa61978137173
schwusch <jonathan.e.m.bocker@gmail.com>
2016-06-14 11:45:15 +0200

    new function

--- server/main.js
+++ server/main.js
@@ -1,5 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  generate_questions();
 });
+
+function generate_questions () {
+  HTTP.call("GET",
+  "http://build.dia.mah.se/pois?latitude=55.595778&longitude=13.014468&within=10000&categories=sights",
+  {}, function(error, response){
+    if(error){
+      console.log(error);
+    } else {
+      JSON.parse(response.content).results[0];
+    }
+  });
+
+}
